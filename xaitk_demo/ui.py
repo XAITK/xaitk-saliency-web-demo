c1a185ac16366fa230b4a7affba506d9758d04bc
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-11-18 13:22:25 -0700

    update ui

--- xaitk_demo/ui.py
+++ xaitk_demo/ui.py
@@ -278,7 +278,17 @@ def xai_viz():
         ),
         "XAI visualization",
     ]
-    _content.children += ["Hello world"]
+    _content.children += [
+        vuetify.VCol([
+            vuetify.VSpacer(),
+        vuetify.VRow([
+        vuetify.VSpacer(),
+        vuetify.VProgressCircular(indeterminate=True, color="primary", size="300", width="15"),
+        vuetify.VSpacer(),
+        ]),
+        vuetify.VSpacer(),
+        ])
+    ]
 
     return _card
 
